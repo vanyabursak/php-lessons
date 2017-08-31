fa67f4a1a8ea0a0ff93db684f454358435bf8137
ivan <bursakivan@gmail.com>
2017-08-31 15:21:48 +0300

    commit

---
+++ String-functions.php
@@ -0,0 +1,11 @@
+<?php
+$string = "This is example!";
+
+echo strlen($string);//число символов
+echo strpos($string,"is",4);//ищем на совпадение
+echo substr($string,4);//возвращает часть строки
+echo str_replace("is","abs",$string);//заменняет часть строки
+echo htmlspecialchars($string);//Преобразует специальные символы в HTML сущности
+echo md5(657);//кодирование строки
+echo strtolower($string);//все слова в малом регистре
+echo strtolower($string);//все слова в малом регистре
