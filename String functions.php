0bc137e5d68830a129552cb1428a453b65eed209
ivan <bursakivan@gmail.com>
2017-08-31 15:07:56 +0300

    commit

--- String functions.php
+++ String functions.php
@@ -4,3 +4,5 @@ $string = "This is example!";
 echo strlen($string);//число символов
 echo strpos($string,"is",4);//ищем на совпадение
 echo substr($string,4);//возвращает часть строки
+echo str_replace("is","abs",$string);//заменняет часть строки
+echo htmlspecialchars($string);//Преобразует специальные символы в HTML сущности
